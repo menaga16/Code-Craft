d9c7edc62adab3c43668c9005ef65e62b0a5bf81
Menaga Raman <116909288+menaga16@users.noreply.github.com>
2024-01-21 21:20:28 +0530

    Sum of digits from a to b.c

---
+++ #include <stdio.h>  int main()  {      int a,b,sum=0;     scanf("%d %d",&a,&b);     int sum1;     for(int i=a;i<=b;i++)     {         sum1=i;         while(sum1>0) {             sum=sum+sum1%10;             sum1/Sum of digits from a to b.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() 
+{
+
+    int a,b,sum=0;
+    scanf("%d %d",&a,&b);
+    int sum1;
+    for(int i=a;i<=b;i++)
+    {
+        sum1=i;
+        while(sum1>0) {
+            sum=sum+sum1%10;
+            sum1/=10;
+        }
+    }
+    printf("%d",sum);
+}
+
+
+/*
+OUTPUT:
+11 12
+5
+
+...Program finished with exit code 0
+Press ENTER to exit console.
+
+*/
